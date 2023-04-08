4385dc705cbcd3d4ff4538217268d9096e7b63bb
Ning An <1196244890@qq.com>
2023-04-08 22:06:37 +0200

    use list

--- grid_search.py
+++ grid_search.py
@@ -4,7 +4,7 @@ import numpy as np
 from constants import *
 from sklearn.model_selection import train_test_split
 
-class grid_search:
+class Grid_search:
     def __init__(self):
         pass
 
@@ -17,44 +17,46 @@ class grid_search:
             predictions = np.array(rank_values, dtype=int)
             predictions = predictions.transpose()
             predictions = {model_name[i]: predictions[i].tolist() for i in range(len(predictions))}
+
         return predictions
+    
 
-
-    def dataset_split(self, predictions) -> list:
-        predictions = np.array(list(predictions.values()))
+    def dataset_split(self, predictions: dict) -> list:
+        predictions = np.transpose(np.array(list(predictions.values())))
         train, test = train_test_split(predictions, test_size=0.3, random_state=42)
+
         return train, test
 
 
-    def calculate_ensemble_score(self, y_pred, models):
-        predictions = self.load_predictions('result/icews14/ranked_quads.json')
+    def calculate_ensemble_score(self, rank_score: np.ndarray, model_weights: list):
         ensemble_score = 0
-        for model_name in models:
-            weight = models[model_name]
-            for i in range(y_pred.ndim):
-                ensemble_score += weight * predictions[model_name][i]
+        for i in range(rank_score.shape[0]):
+            for j in range(rank_score.shape[1]):
+                ensemble_score += model_weights[j] * rank_score[i][j]
         ensemble_score = round(ensemble_score, 2)
+        print(f"updated ensemble score: {ensemble_score}")
 
         return ensemble_score
     
 
-    def grid_search_weight(self, y_pred, models, weight_ranges):
+    def grid_search_weight(self, rank_score: np.ndarray, model_weights: list, weight_ranges: dict):
         best_weights = None
-        init_score = self.calculate_ensemble_score(y_pred, models)
+        init_score = self.calculate_ensemble_score(rank_score, model_weights)
         print("init_score: ", init_score)
-        grid = itertools.product(*[weight_ranges[model] for model in models])
+        grid = itertools.product(*list(weight_ranges.values()))
         for weights in grid:
             if sum(weights) != 1:
                 pass
             else:
-                for i, model_name in enumerate(models):
-                    models[model_name] = weights[i]
-                    ensemble_score = self.calculate_ensemble_score(y_pred, models)
-                    if ensemble_score < init_score:
-                        init_score = ensemble_score
-                        best_weights = weights
-                    else:
-                        best_weights = list(models.values())
+                ensemble_score = self.calculate_ensemble_score(rank_score, list(weights))
+                if ensemble_score < init_score:
+                    init_score = ensemble_score
+                    model_weights = list(weights)
+                    best_weights = model_weights
+                else:
+                    best_weights = model_weights
+                print(f"updated weight: {best_weights}")
         
         return best_weights
+    
 
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from grid_search import grid_search
+from grid_search import Grid_search
+from bayesian_opt import Bayesian_opt
 import numpy as np
 
 
 if __name__ == "__main__":
     # Grid Search
-    models = {
+    init_weight = {
         'DE_TransE': 0.2,
         'DE_SimplE': 0.2,
         'DE_DistMult': 0.2,
@@ -13,19 +14,22 @@ if __name__ == "__main__":
     }
 
     weight_ranges = {
-        'DE_TransE':  [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0],
-        'DE_SimplE': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0],
-        'DE_DistMult': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0],
-        'TERO': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0],
-        'ATISE': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0]
+        'DE_TransE':  [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        'DE_SimplE': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        'DE_DistMult': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        'TERO': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        'ATISE': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     }
 
-    my_grid_search = grid_search()
-    predictions = my_grid_search.load_predictions("temp.json")
+    my_grid_search = Grid_search()
+    predictions = my_grid_search.load_predictions("result/icews14/ranked_quads.json")
     ens_train, ens_test = my_grid_search.dataset_split(predictions)
     print(f"train: {ens_train}")
     print(f"test: {ens_test}")
-    best_weights = my_grid_search.grid_search_weight(ens_train, models, weight_ranges)
+
+    best_weights = my_grid_search.grid_search_weight(ens_train, list(init_weight.values()), weight_ranges)
     print("best weight: ", best_weights)
-    ensemble_score = my_grid_search.calculate_ensemble_score(ens_train, {model_name: best_weights[i] for i, model_name in enumerate(models)})
+
+    ensemble_score = my_grid_search.calculate_ensemble_score(ens_train, best_weights)
     print("ensemble_score: ", ensemble_score)
+    
