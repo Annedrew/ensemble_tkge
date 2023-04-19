61591169aea2a6f2cdd89d1d4b743fe32335a312
Ning An <1196244890@qq.com>
2023-04-19 08:29:14 +0200

    save result

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bayesian_opt import Bayesian_opt
 import time
 from arguments import *
 from constants import *
+from save_result import *
 
 if __name__ == "__main__":
     args = init_args()
@@ -16,7 +17,8 @@ if __name__ == "__main__":
             'ATISE': 0.2
         }
         my_grid_search = Grid_search()
-        predictions = my_grid_search.load_predictions("result/icews14/ranked_quads.json")
+        predictions = my_grid_search.load_predictions("/Users/dutao/Documents/github/ensemble_tkge/results/icews14/ranked_quads.json")
+        # predictions = my_grid_search.load_predictions("results/icews14/ranked_quads.json")
         ens_train, ens_test = my_grid_search.dataset_split(predictions)
 
         start_time = time.time()
@@ -26,11 +28,15 @@ if __name__ == "__main__":
         ensemble_score = my_grid_search.calculate_ensemble_score(ens_train, best_weights)
         print("ensemble_score: ", ensemble_score)
         time_grid = time.time()
-        print(f"The running time for grid search: {round((time_grid - start_time), 2)}s")
+        run_time = round((time_grid - start_time), 2)
+        
     # Bayesian Optimization
     elif args.method == "bayes":
         start_time = time.time()
         my_bayesian_opt = Bayesian_opt()
         best_weights = my_bayesian_opt.bayesian_opt_weight()
+        ensemble_score = my_bayesian_opt.objective_function(best_weights[0], best_weights[1], best_weights[2], best_weights[3], best_weights[4])
         time_bayes = time.time()
-        print(f"The running time for bayesian optimization search: {round((time_bayes - start_time), 2)}s")
+        run_time = round((time_bayes - start_time), 2)
+        
+    save_file(best_weights, run_time, ensemble_score, args)
---
+++ save_result.py
@@ -0,0 +1,41 @@
+import os
+
+def save_file(best_weights, run_time, ensemble_score, args):
+    if args.method == "grid":
+        file_name = "grid_results.txt"
+        if os.path.exists(file_name):
+            with open(file_name, "a") as f:
+                run_time = f"Running time: {run_time}s"
+                best_weights = f"DE_TransE: {best_weights[0]}, DE_SimplE: {best_weights[1]}, DE_DistMult: {best_weights[2]}, TERO: {best_weights[3]}, ATISE: {best_weights[4]}"
+                # ensemble_score = f"HITS@1: {}"
+                f.write("______RUN______" + "\n")
+                f.write(run_time + "\n")
+                f.write(best_weights + "\n")
+        else:
+            with open(file_name, "w") as f:
+                run_time = f"Running time: {run_time}s"
+                best_weights = f"DE_TransE: {best_weights[0]}, DE_SimplE: {best_weights[1]}, DE_DistMult: {best_weights[2]}, TERO: {best_weights[3]}, ATISE: {best_weights[4]}"
+                # ensemble_score = f"HITS@1: {}"
+                f.write("\n" + "______RUN______" + "\n")
+                f.write(run_time + "\n")
+                f.write(best_weights + "\n")
+    elif args.method == "bayes:":
+        file_name = "bayes_results.txt"
+        if os.path.exists(file_name):
+            with open(file_name, "a") as f:
+                run_time = f"Running time: {run_time}s"
+                best_weights = f"DE_TransE: {best_weights[0]}, DE_SimplE: {best_weights[1]}, DE_DistMult: {best_weights[2]}, TERO: {best_weights[3]}, ATISE: {best_weights[4]}"
+                # ensemble_score = f"HITS@1: {}"
+                f.write("\n" + "______RUN______" + "\n")
+                f.write(run_time + "\n")
+                f.write(best_weights + "\n")
+        else:
+            with open(file_name, "w") as f:
+                run_time = f"Running time: {run_time}s"
+                best_weights = f"DE_TransE: {best_weights[0]}, DE_SimplE: {best_weights[1]}, DE_DistMult: {best_weights[2]}, TERO: {best_weights[3]}, ATISE: {best_weights[4]}"
+                # ensemble_score = f"HITS@1: {}"
+                f.write("\n" + "______RUN______" + "\n")
+                f.write(run_time + "\n")
+                f.write(best_weights + "\n")
+    else:
+        pass
