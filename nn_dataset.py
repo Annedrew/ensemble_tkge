5ca39af35aac383e68d9630455ef84ecf13a5ecc
Ning An <1196244890@qq.com>
2023-05-22 08:58:20 +0200

    change the way read json file, the file is too big too read, now it is so much better.

--- nn_dataset.py
+++ nn_dataset.py
@@ -1,5 +1,7 @@
 # Build the training and dataset for nn
 import json
+# need to choose the interpreter in conda by hand.
+import ijson
 import csv
 
 class NNDataset:
@@ -17,33 +19,42 @@ class NNDataset:
     def get_input(self, file_path, model_name):
         with open(file_path, "r") as f:
             # TODO: Change the key of simulated score to "SIMU", for now both scores and ranks are called "RANK"
-            data = json.load(f)
             simu_score_dict = {}
             for name in model_name:
                 simu_score_dict[name] = []
-                
             rows = []
-            for i in range(len(data)):
-                for name in model_name:
-                    if name == "DE_TransE":
-                        top_5 = [top[1] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_score_dict[name] = top_5
-                    elif name == "DE_SimplE":
-                        top_5 = [top[1] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_score_dict[name] = top_5
-                    elif name == "DE_DistMult":
-                        top_5 = [top[1] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_score_dict[name] = top_5
-                    elif name == "TERO":
-                        top_5 = [top[1] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_score_dict[name] = top_5
-                    elif name == "ATISE":
-                        top_5 = [top[1] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_score_dict[name] = top_5
+
+            # The file is too big, use ijson to parse the data incrementally
+            objects = ijson.items(f, "item")
+            i = 0
+            for obj in objects:
+                i += 1
+                if i % 100 == 0:
+                    print(f"Fetching the score of query: {str(i)}")
+                if "DE_TransE" in obj["RANK"]:
+                    value = obj["RANK"]["DE_TransE"]
+                    top_5 = [top[1] for top in self.get_top_5((json.loads(value)))]
+                    simu_score_dict["DE_TransE"] = top_5
+                if "DE_SimplE" in obj["RANK"]:
+                    value = obj["RANK"]["DE_SimplE"]
+                    top_5 = [top[1] for top in self.get_top_5((json.loads(value)))]
+                    simu_score_dict["DE_SimplE"] = top_5
+                if "DE_DistMult" in obj["RANK"]:
+                    value = obj["RANK"]["DE_DistMult"]
+                    top_5 = [top[1] for top in self.get_top_5((json.loads(value)))]
+                    simu_score_dict["DE_DistMult"] = top_5
+                if "TERO" in obj["RANK"]:
+                    value = obj["RANK"]["TERO"]
+                    top_5 = [top[1] for top in self.get_top_5((json.loads(value)))]
+                    simu_score_dict["TERO"] = top_5
+                if "ATISE" in obj["RANK"]:
+                    value = obj["RANK"]["ATISE"]
+                    top_5 = [top[1] for top in self.get_top_5((json.loads(value)))]
+                    simu_score_dict["ATISE"] = top_5
+                    
                 row = []
                 for name in model_name:
                     row += simu_score_dict[name]
-
                 rows.append(row)
 
         return rows
@@ -51,55 +62,62 @@ class NNDataset:
 
     def get_input_id(self, file_path, model_name):
         with open(file_path, "r") as f:
-            data = json.load(f)
             simu_id_dict = {}
             for name in model_name:
                 simu_id_dict[name] = []
-                
             rows = []
-            for i in range(len(data)):
-                for name in model_name:
-                    if name == "DE_TransE":
-                        top_5 = [top[0] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_id_dict[name] = top_5
-                    elif name == "DE_SimplE":
-                        top_5 = [top[0] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_id_dict[name] = top_5
-                    elif name == "DE_DistMult":
-                        top_5 = [top[0] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_id_dict[name] = top_5
-                    elif name == "TERO":
-                        top_5 = [top[0] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_id_dict[name] = top_5
-                    elif name == "ATISE":
-                        top_5 = [top[0] for top in self.get_top_5(json.loads(data[i]["RANK"][name]))]
-                        simu_id_dict[name] = top_5
+
+            objects = ijson.items(f, "item")
+            i = 0
+            for obj in objects:
+                i += 1
+                if i % 100 == 0:
+                    print(f"Fetching the id of query: {str(i)}")
+                if "DE_TransE" in obj["RANK"]:
+                    value = obj["RANK"]["DE_TransE"]
+                    top_5 = [top[0] for top in self.get_top_5((json.loads(value)))]
+                    simu_id_dict["DE_TransE"] = top_5
+                if "DE_SimplE" in obj["RANK"]:
+                    value = obj["RANK"]["DE_SimplE"]
+                    top_5 = [top[0] for top in self.get_top_5((json.loads(value)))]
+                    simu_id_dict["DE_SimplE"] = top_5
+                if "DE_DistMult" in obj["RANK"]:
+                    value = obj["RANK"]["DE_DistMult"]
+                    top_5 = [top[0] for top in self.get_top_5((json.loads(value)))]
+                    simu_id_dict["DE_DistMult"] = top_5
+                if "TERO" in obj["RANK"]:
+                    value = obj["RANK"]["TERO"]
+                    top_5 = [top[0] for top in self.get_top_5((json.loads(value)))]
+                    simu_id_dict["TERO"] = top_5
+                if "ATISE" in obj["RANK"]:
+                    value = obj["RANK"]["ATISE"]
+                    top_5 = [top[0] for top in self.get_top_5((json.loads(value)))]
+                    simu_id_dict["ATISE"] = top_5
+
                 row = []
                 for name in model_name:
                     row += simu_id_dict[name]
-
                 rows.append(row)
 
         return rows
-        
 
         
-    def save_csv(self, simu_score, model_name, id_score):
+    def save_csv(self, file_name, rows, model_name, id_score):
         row_name = []
         for name in model_name:
             for i in range(len(model_name)):
                 row_name.append(f"{name}_{i+1}")
         if id_score == "ID":
-            with open("top_5_id.csv", "w") as f:
+            with open(file_name, "w") as f:
                 writer = csv.writer(f)
                 writer.writerow(row_name)
-                for row in simu_score:
+                for row in rows:
                     writer.writerow(row)
         elif id_score == "Score":
-            with open("top_5_score.csv", "w") as f:
+            with open(file_name, "w") as f:
                 writer = csv.writer(f)
                 writer.writerow(row_name)
-                for row in simu_score:
+                for row in rows:
                     writer.writerow(row)
 
 
@@ -113,10 +131,22 @@ if __name__ == "__main__":
     model_name = ["DE_TransE", "DE_SimplE", "DE_DistMult", "TERO", "ATISE"]
 
     dataset = NNDataset()
-
-    simu_score = dataset.get_input("new_results/temp_sim_scores.json", model_name)
-    simu_id = dataset.get_input_id("new_results/temp_sim_scores.json", model_name)
     id_score = ["ID", "Score"]
 
-    # dataset.save_csv(simu_id, model_name, id_score[0])
-    # dataset.save_csv(simu_score, model_name, id_score[1])
+    # Test code
+    # simu_score = dataset.get_input("new_results/temp_sim_scores.json", model_name)
+    # dataset.save_csv("new_results/temp_top_5_score.csv", simu_score, model_name, id_score[1])
+    # simu_id = dataset.get_input_id("new_results/temp_sim_scores.json", model_name)
+    # dataset.save_csv("new_results/temp_top_5_id.csv", simu_id, model_name, id_score[0])
+
+    # Get the input for training 
+    simu_score = dataset.get_input("new_results/query_ens_train_sim_scores.json", model_name)
+    dataset.save_csv("new_results/ens_train_top_5_score.csv", simu_score, model_name, id_score[1])
+    simu_id = dataset.get_input_id("new_results/query_ens_train_sim_scores.json", model_name)
+    dataset.save_csv("new_results/ens_train_top_5_id.csv", simu_id, model_name, id_score[0])
+
+    # Get the input for testing
+    # simu_score = dataset.get_input("new_results/query_ens_test_sim_scores.json", model_name)
+    # dataset.save_csv("new_results/ens_test_top_5_score.csv", simu_score, model_name, id_score[1])
+    # simu_id = dataset.get_input_id("new_results/query_ens_test_sim_scores.json", model_name)
+    # dataset.save_csv("new_results/ens_test_top_5_id.csv", simu_id, model_name, id_score[0])
