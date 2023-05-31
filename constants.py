666ea650e0caa8d6fd0bdedf15ce9dd81d1a3e63
Ning An <1196244890@qq.com>
2023-05-31 10:36:30 +0200

    clean up constants

--- constants.py
+++ constants.py
@@ -10,20 +10,48 @@ WEIGHT_RANGE = {
 # Constants for bayesian optimization
 BOUNDS = {'w1': (0.1, 0.5), 'w2': (0.5, 0.8), 'w3': (0.8, 1.0), 'w4': (0.8, 1.0), 'w5': (0.8, 1.0)}
 
-# 4*5 1*5
-# # Constants for neural network
-# INPUT_SIZE = 20 # 4 * 5
-# OUTPUT_SIZE = 5 # 1 * 5
-# HIDDEN_SIZE = 25
-# EPOCH = 200#500
-# LEARNING_RATE = 0.01
-# BATCH_SIZE = 32
-
-# 1*5 1*5
 # Constants for neural network
+# Input: 5*5 (5 = top5 of predicted score) (5 = 5 models)
+# Output: 1*5 (1 = correct score) (5 = 5 models)
+# INPUT_SIZE = 25
+# OUTPUT_SIZE = 5
+# HIDDEN_SIZE = 25
+# EPOCH = 100
+# LEARNING_RATE = 0.001
+# BATCH_SIZE = 64
+
+# Input: 5*5 (5 = scores of 5 entities, e.g. the first score is the score of the first replaced entity) (5 = 5 models)
+# Output: 1*5 (1 = 1) (5 = 5 entities, if the entity is correct answer, then assign 1, else assign 0)
+# INPUT_SIZE = 25
+# OUTPUT_SIZE = 5
+# HIDDEN_SIZE = 25
+# EPOCH = 100
+# LEARNING_RATE = 0.001
+# BATCH_SIZE = 64
+
+# Input: 4*5 (4 = score of h, r, t, T) (5 = 5 models)
+# Output: 1*5 (1 = correct score) (5 = 5 models)
+# INPUT_SIZE = 20
+# OUTPUT_SIZE = 5
+# HIDDEN_SIZE = 20
+# EPOCH = 200
+# LEARNING_RATE = 0.001
+# BATCH_SIZE = 64
+
+# Input: 1*5 (1 = score of h) (5 = 5 models)
+# Output: 1*5 (1 = correct score) (5 = 5 models)
+# INPUT_SIZE = 5
+# OUTPUT_SIZE = 5
+# HIDDEN_SIZE = 5
+# EPOCH = 400 #500
+# LEARNING_RATE = 0.001
+# BATCH_SIZE = 64
+
+# Input: 1*5 (1 = score of simulated fact) (5 = 5 models)
+# Output: 1*5 (1 = the rank of correct score) (5 = 5 models)
 INPUT_SIZE = 5
 OUTPUT_SIZE = 5
 HIDDEN_SIZE = 5
-EPOCH = 80#500
+EPOCH = 100 #500
 LEARNING_RATE = 0.001
-BATCH_SIZE = 64
+BATCH_SIZE = 256
